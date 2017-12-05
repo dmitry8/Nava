1afc6278888d59b31f56889ae2a7bdd18b27a993
Dmitry <dmitrythomas8@gmail.com>
2017-12-05 19:44:36 +0300

    Добавляет webp изображения и пару иконок

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task("images", function() {
 gulp.task("webp", function () {
   return gulp.src("img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("img"));
+    .pipe(gulp.dest("build/img"));
 })
 
 gulp.task("clean", function () {
